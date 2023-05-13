046ae1785465ab0e8781da65b4b69a0774ed408b
sukabing <2113097922@qq.com>
2023-05-13 15:55:28 +0800

    update

--- gdx-game-android/gen/com/sukabing/linkingtheworld/BuildConfig.java
+++ gdx-game-android/gen/com/sukabing/linkingtheworld/BuildConfig.java
@@ -3,4 +3,8 @@ package com.sukabing.linkingtheworld;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
+    public final static String APPLICATION_ID = "com.sukabing.linkingtheworld";
+    public final static String BUILD_TYPE = "debug";
+    public final static long VERSION_CODE = 1;
+    public final static String VERSION_NAME = "1.0";
 }
